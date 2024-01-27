0c86cf8efde942b611deb8954f9e47754ba646b5
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-27 16:32:44 +0100

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,14 +1,53 @@
-const API_URL ="https://striveschool-api.herokuapp.com/api/product/";
-export async function fetchProductById= (productID) =>{
-    const response = await fetch(`${API_URL}/${productID}`, {
-        headers:{
-            authorization:
-            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
-
-        },
-    }
-    );
-    const product = await response.json();
-    return product;
+const API_URL = "https://striveschool-api.herokuapp.com/api/product/";
+export async function fetchProductById(productId) {
+  const response = await fetch(`${API_URL}/${productId}`, {
+    headers: {
+      authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+    },
+  });
+  const product = await response.json();
+  return product;
+}
+export async function fetchProducts() {
+  const response = await fetch(API_URL, {
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+    },
+  });
+  const products = await response.json();
+  return products;
 }
 
+export async function deleteProduct(productId) {
+  const response = await fetch(`${API_URL}/${productId}`, {
+    method: "DELETE",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+    },
+  });
+
+  const deletedProduct = await response.json();
+  console.log("Prodotto eliminato", deletedProduct);
+  return deletedProduct;
+}
+export async function updatedProduct(productId, updatedData) {
+  const response = await fetch(`${API_URL}/${productId}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+    },
+    body: JSON.stringify(updatedData),
+  });
+  if (!response.ok) {
+    throw new Error("Errrore  server");
+  }
+
+  const updatedData = await response.json();
+  console.log("Prodotto aggiornato", resposeData);
+  return updatedData;
+}
