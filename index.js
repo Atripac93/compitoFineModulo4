8d38307cc8ebfd9e204cec2ae5a1e77345a2f698
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-27 13:10:13 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import { fetchProducts } from "./fetch.js";
+import { renderProductCards, handleAddProductForm } from "./components.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const products = await fetchProducts();
+  renderProductsCards(products);
+  handleAddProductForm();
+});
