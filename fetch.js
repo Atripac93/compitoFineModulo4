7e5bbffb0e47ab166ae90e7eb90f495bac06aee7
Carlo Atripaldi <carloatrica@gmail.com>
2024-01-27 17:01:51 +0100

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,19 +1,72 @@
+// const API_URL = "https://striveschool-api.herokuapp.com/api/product/";
+// export async function fetchProductById(productId) {
+//   const response = await fetch(`${API_URL}/${productId}`, {
+//     headers: {
+//       authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+//     },
+//   });
+//   const product = await response.json();
+//   return product;
+// }
+// export async function fetchProducts() {
+//   const response = await fetch(API_URL, {
+//     headers: {
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+//     },
+//   });
+//   const products = await response.json();
+//   return products;
+// }
+
+// export async function deleteProduct(productId) {
+//   const response = await fetch(`${API_URL}/${productId}`, {
+//     method: "DELETE",
+//     headers: {
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+//     },
+//   });
+
+//   const deletedProduct = await response.json();
+//   console.log("Prodotto eliminato", deletedProduct);
+//   return deletedProduct;
+// }
+// export async function updatedProduct(productId, updatedData) {
+//   const response = await fetch(`${API_URL}/${productId}`, {
+//     method: "PUT",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization:
+//         "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+//     },
+//     body: JSON.stringify(updatedData),
+//   });
+//   if (!response.ok) {
+//     throw new Error("Errrore  server");
+//   }
+
+//   const updatedData = await response.json();
+//   console.log("Prodotto aggiornato", resposeData);
+//   return updatedData;
+// }
 const API_URL = "https://striveschool-api.herokuapp.com/api/product/";
+
 export async function fetchProductById(productId) {
   const response = await fetch(`${API_URL}/${productId}`, {
     headers: {
-      authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+      Authorization: "Bearer your_access_token_here",
     },
   });
   const product = await response.json();
   return product;
 }
+
 export async function fetchProducts() {
   const response = await fetch(API_URL, {
     headers: {
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+      Authorization: "Bearer your_access_token_here",
     },
   });
   const products = await response.json();
@@ -29,25 +82,30 @@ export async function deleteProduct(productId) {
     },
   });
 
+  if (!response.ok) {
+    throw new Error("Errore durante l'eliminazione del prodotto");
+  }
+
   const deletedProduct = await response.json();
-  console.log("Prodotto eliminato", deletedProduct);
+  console.log("Prodotto eliminato:", deletedProduct);
   return deletedProduct;
 }
-export async function updatedProduct(productId, updatedData) {
+
+export async function updateProduct(productId, updatedData) {
   const response = await fetch(`${API_URL}/${productId}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWFmYzVjZTczMjBjNjAwMThiOGYwMjEiLCJpYXQiOjE3MDYwMTgyNTQsImV4cCI6MTcwNzIyNzg1NH0.adHhNogNz7-k_4h2Ybx9AUqGFW2wG1785jJPSzE1d-E",
+      Authorization: "Bearer your_access_token_here",
     },
     body: JSON.stringify(updatedData),
   });
+
   if (!response.ok) {
-    throw new Error("Errrore  server");
+    throw new Error("Errore durante l'aggiornamento del prodotto");
   }
 
-  const updatedData = await response.json();
-  console.log("Prodotto aggiornato", resposeData);
-  return updatedData;
+  const responseData = await response.json();
+  console.log("Prodotto aggiornato:", responseData);
+  return responseData;
 }
